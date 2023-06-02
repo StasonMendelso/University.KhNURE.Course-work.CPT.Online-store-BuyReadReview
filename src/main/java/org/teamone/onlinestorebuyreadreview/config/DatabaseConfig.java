f4e095fb08f7e9c0c81f8d81a9596fd25f4422fd
Stanislav Hlova <stanislav.hlova@gmail.com>
2023-06-02 10:24:01 +0300

    config: add basic config for database connectivity
    Add basic configuration for database connectivity using JdbcTemplate

---
+++ src/main/java/org/teamone/onlinestorebuyreadreview/config/DatabaseConfig.java
@@ -0,0 +1,35 @@
+package org.teamone.onlinestorebuyreadreview.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+/**
+ * @author Stanislav Hlova
+ */
+@Configuration
+@RequiredArgsConstructor
+public class DatabaseConfig {
+
+    private final Environment environment;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getRequiredProperty("spring.datasource.driver-class-name"));
+        dataSource.setUrl(environment.getRequiredProperty("spring.datasource.url"));
+        dataSource.setUsername(environment.getRequiredProperty("spring.datasource.username"));
+        dataSource.setPassword(environment.getRequiredProperty("spring.datasource.password"));
+
+        return dataSource;
+    }
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource){
+        return new JdbcTemplate(dataSource);
+    }
+}
